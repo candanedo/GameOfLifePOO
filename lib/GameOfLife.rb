f70187db67fcc5cac01b6f277012697ad1fafb0d
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-09-27 19:56:00 -0500

    This is the final version of the game of life

--- lib/GameOfLife.rb
+++ lib/GameOfLife.rb
@@ -1,9 +1,10 @@
 require_relative './lib/interface'
 class Game
-        def game_on
-                game = Interface.new
-                game.start
-        end
+  def game_on
+    game = Interface.new
+    game.start
+  end
 end
-#g = Game.new
-#g.game_on
+
+g = Game.new
+g.game_on
